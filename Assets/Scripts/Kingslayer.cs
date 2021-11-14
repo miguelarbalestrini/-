64009774f32e927b506f15db496bf72507b41f6e
Miguel Angel Rodenas Balestrini <miguel_angel.rb@outlook.com>
2021-11-14 00:38:26 -0300

    kingslayer added

---
+++ Assets/Scripts/Kingslayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kingslayer : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField]
+    private Weapon weapon;
+
+    #endregion
+
+    #region UnityMethods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #endregion
+
+    #region PrivateMethods
+
+    #endregion
+
+    #region PublicMethods
+
+    public void pickWeapon(Weapon newWeapon)
+    {
+
+    }
+
+    #endregion
+}
